a7bc9b1fdf74a2902268ea4f4effa5cd6dc486d1
SUNG WOOK HWANG <destiny0810@naver.com>
2021-02-16 14:39:59 +0900

    코드 삭제

--- src/ch01/Hello.java
+++ src/ch01/Hello.java
@@ -3,10 +3,6 @@ package ch01;
 public class Hello {
 
 	public static void main(String[] args) {
-		System.out.println("HELLO JAVA!");
-		System.out.println("HELLO JAVA!");
-		System.out.println("HELLO JAVA!");
-		System.out.println("HELLO JAVA");
 		System.out.println("Hello World");
 	}
 
