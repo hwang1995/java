0027c5d659185b0cb6d9ca87e968a110575b79c5
SUNG WOOK HWANG <destiny0810@naver.com>
2021-02-22 16:06:44 +0900

    21/02/22 PM 04:06 중간 업데이트

---
+++ src/ch06/exam14/Example.java
@@ -0,0 +1,13 @@
+package ch06.exam14;
+
+public class Example {
+
+	public static void main(String[] args) {
+		System.out.println(Korean.NATION);
+		Korean k1 = new Korean("123456-1234567");
+		System.out.println(Math.PI);
+		double area  = 10 * 10 * Math.PI;
+		System.out.println(area);
+	}
+
+}
---
+++ src/ch06/exam14/Korean.java
@@ -0,0 +1,19 @@
+package ch06.exam14;
+
+public class Korean {
+	// Field
+	static final String NATION = "한국"; 
+	final String ssn;
+	
+	// final static이 들어가면 필드 이름을 대문자로 하는게 관례
+	// Constructor
+	Korean(String ssn){
+		this.ssn  = ssn;
+	}
+	
+	
+	// Method
+	
+	
+
+}
